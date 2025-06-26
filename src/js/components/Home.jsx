e02520287e95181d7617cf73395f6f272a8422cb
Julián David Pineda Betancurth <89669806+juliandavidpineda@users.noreply.github.com>
2025-06-26 04:12:05 +0000

    :sparkles: corrección nombre de variables

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -11,9 +11,9 @@ const Home = () => {
 		<div className="text-center">
 			<div className="cable"></div>
 			<div className="trafficLight">
-				<div className={"luz red " + (lightColor=="red"? "on": "" )} onClick={()=>setLightColor("red")}></div>
-				<div className={"luz yellow " + (lightColor=="yellow"? "on": "" )} onClick={()=>setLightColor("yellow")}></div>
-				<div className={"luz green " + (lightColor=="green"? "on": "" )} onClick={()=>setLightColor("green")}></div>
+				<div className={"ligth red " + (lightColor=="red"? "on": "" )} onClick={()=>setLightColor("red")}></div>
+				<div className={"ligth yellow " + (lightColor=="yellow"? "on": "" )} onClick={()=>setLightColor("yellow")}></div>
+				<div className={"ligth green " + (lightColor=="green"? "on": "" )} onClick={()=>setLightColor("green")}></div>
 			</div>
 			
         
