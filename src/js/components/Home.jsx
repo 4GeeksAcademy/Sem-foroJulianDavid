5af3f13a5eb06bcd68fa627c43b7128dcc54bb40
Julián David Pineda Betancurth <89669806+juliandavidpineda@users.noreply.github.com>
2025-06-26 05:05:12 +0000

    :zap: newButton

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -7,16 +7,26 @@ const Home = () => {
 
 	const [lightColor, setLightColor] = useState("amarillo");
 
+	const colors = ["red", "yellow", "green"]; 
+
+	const toggleLight = () => {
+		const currentIndex = colors.indexOf(lightColor);
+		const nextIndex = (currentIndex + 1) % colors.length;
+		setLightColor(colors[nextIndex]);
+	};
+
 	return (
 		<div className="text-center">
 			<div className="cable"></div>
 			<div className="trafficLight">
-				<div className={"ligth red " + (lightColor=="red"? "on": "" )} onClick={()=>setLightColor("red")}></div>
-				<div className={"ligth yellow " + (lightColor=="yellow"? "on": "" )} onClick={()=>setLightColor("yellow")}></div>
-				<div className={"ligth green " + (lightColor=="green"? "on": "" )} onClick={()=>setLightColor("green")}></div>
+				<div className={"light red " + (lightColor == "red" ? "on" : "")} onClick={() => setLightColor("red")}></div>
+				<div className={"light yellow " + (lightColor == "yellow" ? "on" : "")} onClick={() => setLightColor("yellow")}></div>
+				<div className={"light green " + (lightColor == "green" ? "on" : "")} onClick={() => setLightColor("green")}></div>
 			</div>
 			
-        
+			<button
+				className="btn btn-dark mt-5" onClick={toggleLight}> Chance trafficLight</button>
+
 		</div>
 	);
 };
